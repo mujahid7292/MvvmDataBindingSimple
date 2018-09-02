36ceceef0eaf8a29466cef7c53e17f62d5e1bb51
Saifullah Al Mujahid <mujahid7292@gmail.com>
2018-09-02 12:17:24 +0600

    2nd Commit

--- app/src/main/java/sand/www/mvvmdatabindingsimple/MainActivity.java
+++ app/src/main/java/sand/www/mvvmdatabindingsimple/MainActivity.java
@@ -1,13 +1,18 @@
 package sand.www.mvvmdatabindingsimple;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import sand.www.mvvmdatabindingsimple.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
     }
 }
